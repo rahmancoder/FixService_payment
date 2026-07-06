ca056d7fd31f7911172e49a6fad0c896b0ae96dc
mustafiz <sayhemrahman@gmail.com>
2026-07-07 01:49:43 +0600

    multi schema model user, technicianprofile, category and service with relation added

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,10 +4,13 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+
+  schema: "prisma/schema",
+
   migrations: {
     path: "prisma/migrations",
   },
+
   datasource: {
     url: process.env["DATABASE_URL"],
   },
