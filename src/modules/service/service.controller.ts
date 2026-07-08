c902b9154dcdd3030f42efd5623c1d05dea68a2e
mustafiz <sayhemrahman@gmail.com>
2026-07-08 06:32:13 +0600

    service with categoryId created and added filter searching sorting for getAllservices

--- src/modules/service/service.controller.ts
+++ src/modules/service/service.controller.ts
@@ -3,30 +3,41 @@ import { NextFunction, Request, Response } from "express";
 
 import httpStatus from "http-status";
 import { serviceService } from "./service.service";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 
-const getAllServices = async (req: Request, res: Response) => {
-    try {
-        const services = await serviceService.getAllServicesFromDB();
-        res.status(200).json({
-            success: true,
-            statusCode: 200,
-            message: "Services retrieved successfully",
-            data: services
-        });
-    }
+//01
+const createService = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const userId = req.user!.id
+    const payload = req.body;
+    const result = await serviceService.createServiceIntoDB(userId, payload);
+    sendResponse(res, {
+        statusCode: httpStatus.CREATED,
+        success: true,
+        message: 'Service created successfully',
+        data: result,
+    });
+});
 
 
-    catch (error) {
-        console.error("Error retrieving services:", error);
-        res.status(500).json({
-            success: false,
-            statusCode: 500,
-            message: "Failed to retrieve services",
-            error: (error as Error).message
-        });
-    }
-};
+
+//02
+const getAllServices = catchAsync(async (req: Request, res: Response) => {
+
+
+    const services = await serviceService.getAllServicesFromDB(req.query);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Services retrieved successfully',
+        meta: services.meta,
+        data: services.data,
+    });
+});
+
+
 
 export const serviceController = {
-    getAllServices
+    getAllServices,
+    createService
 };
--- src/modules/service/service.service.ts
+++ src/modules/service/service.service.ts
@@ -1,9 +1,138 @@
+import { prisma } from "../../lib/prisma";
+
+import ApiError from "../../middlewares/ApiError";
+import httpStatus from "http-status";
+import { paginationHelper } from "../../utils/paginationHelper";
+import { Prisma } from "../../../generated/prisma/client";
 
 
-const getAllServicesFromDB = async () => {
-    // Implementation for fetching all services from the database
-}
+type IServiceFilters = {
+    searchTerm?: string;
+    categoryId?: number;
+
+    page?: string;
+    limit?: string;
+    sortBy?: string;
+    sortOrder?: 'asc' | 'desc';
+};
+
+
+// 01
+const createServiceIntoDB = async (
+    technicianUserId: string,
+    payload: {
+        title: string;
+        description?: string;
+        price: number;
+        categoryId: number;
+        location?: string;
+    }
+) => {
+    const technicianProfile = await prisma.technicianProfile.findUnique({
+        where: { userId: technicianUserId },
+    });
+
+    if (!technicianProfile) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'Technician profile not found');
+    }
+
+    const category = await prisma.category.findUnique({ where: { id: payload.categoryId } });
+    if (!category) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'Category not found');
+    }
+
+    return prisma.service.create({
+        data: { ...payload, technicianId: technicianProfile.id },
+    });
+};
+
+
+//02
+
+const getAllServicesFromDB = async (filters: IServiceFilters) => {
+
+    const { searchTerm, categoryId, ...paginationOptions } = filters;
+
+    const { page, limit, skip, sortBy, sortOrder } = paginationHelper.calculatePagination(paginationOptions);
+
+    const andConditions: Prisma.ServiceWhereInput[] = [
+        {
+            isActive: true
+        }
+    ];
+
+    if (searchTerm) {
+        andConditions.push({
+            OR: [
+                {
+                    title: {
+                        contains: searchTerm,
+                        mode: 'insensitive'
+                    }
+                },
+
+                {
+                    description:
+                    {
+                        contains: searchTerm,
+                        mode: 'insensitive'
+                    }
+                },
+            ],
+        });
+    }
+
+    if (categoryId) andConditions.push({ categoryId });
+
+
+
+
+    const whereConditions: Prisma.ServiceWhereInput = { AND: andConditions };
+
+    const result = await prisma.service.findMany({
+        where: whereConditions,
+        skip,
+        take: limit,
+        orderBy:
+        {
+            [sortBy]: sortOrder
+        },
+
+        include:
+        {
+            category: true,
+            technician:
+            {
+                include:
+                {
+                    user:
+                    {
+                        select:
+
+                        {
+                            id: true,
+                            name: true,
+                        }
+
+                    }
+                }
+            },
+        },
+    });
+
+    const total = await prisma.service.count({ where: whereConditions });
+
+    return {
+        meta: { page, limit, total },
+        data: result
+    };
+};
+
+
+
+
 
 export const serviceService = {
+    createServiceIntoDB,
     getAllServicesFromDB
 };
